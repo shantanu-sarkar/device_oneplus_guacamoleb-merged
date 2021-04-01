19ca0bf2b80bbaf17f08242940d785adf1c45e40
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-04-01 22:16:16 +0530

    guacamoleb: Uprev radio to 1.5
    Change-Id: I8bfe34a4fcd19aa8b8a410049e5123a3e34a586b

--- device.mk
+++ device.mk
@@ -410,9 +410,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.2 \
     android.hardware.radio@1.5.vendor \
-    android.hardware.radio.config@1.0 \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.0 \
