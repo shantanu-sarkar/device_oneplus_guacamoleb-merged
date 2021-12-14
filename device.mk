f4228a0bfae830ce9eb8804ce91e3033135d21be
LuK1337 <priv.luk@gmail.com>
2021-12-14 11:42:44 +0100

    guacamoleb: Build remaining RIL libs into vendor
    Change-Id: I1db79176ac103cb7fc8db0fa4b4cfc699e38ad49

--- device.mk
+++ device.mk
@@ -410,7 +410,9 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.4 \
     android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.1 \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.0 \
