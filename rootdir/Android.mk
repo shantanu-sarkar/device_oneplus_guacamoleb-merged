0f4470e9d5548d7df9a157d21862cf5fedb6f280
Shantanu Sarkar <shantanuplussarkar@gmail.com>
2022-11-23 22:32:30 +0530

    merge guacamoleb

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,13 +1,19 @@
 LOCAL_PATH := $(call my-dir)
 
-# Common config scripts
-
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ftm_power_config.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ftm_power_config.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
